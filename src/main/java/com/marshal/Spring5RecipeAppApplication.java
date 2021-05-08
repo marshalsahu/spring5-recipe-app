6ff399b0368ed5422f67af3e3779fe37bf0b85d7
Marshal Sahu <sahumarshal@gmail.com>
2021-05-09 03:49:19 +0530

    Assignment completed

--- src/main/java/com/marshal/Spring5RecipeAppApplication.java
+++ src/main/java/com/marshal/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package com.marshal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/marshal/Spring5RecipeAppApplicationTests.java
+++ src/test/java/com/marshal/Spring5RecipeAppApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package com.marshal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
