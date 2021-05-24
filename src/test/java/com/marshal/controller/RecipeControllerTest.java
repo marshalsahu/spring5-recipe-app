10a30aa9d972b8f9c947cd25392fdd27c3a4f58b
Marshal Sahu <sahumarshal@gmail.com>
2021-05-24 06:33:45 +0530

    internationalization and jsr annotation added for validation

--- src/test/java/com/marshal/controller/RecipeControllerTest.java
+++ src/test/java/com/marshal/controller/RecipeControllerTest.java
@@ -1,5 +1,6 @@
 package com.marshal.controller;
 
+import com.marshal.commands.RecipeCommand;
 import com.marshal.domain.Recipe;
 import com.marshal.exceptions.NothingFoundException;
 import com.marshal.service.RecipeService;
@@ -7,10 +8,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -45,6 +48,42 @@ public class RecipeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    public void testPostNewRecipeForm() throws Exception{
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id","")
+                .param("description","some string")
+                .param("directions","some string")
+
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/1/show/"));
+
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception{
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("description","some string")
+                .param("id","")
+        )
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"));
+
     }
 
     @Test
